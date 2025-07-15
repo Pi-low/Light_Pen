d8e167435437f443a03c5426a9fed732d0e3c13f
Nello <n.chommanivong@actia-3e.com>
2025-07-15 17:39:30 +0200

    fix CRGB leds definition

--- ESP_lightstick.ino
+++ ESP_lightstick.ino
@@ -2,13 +2,13 @@
 #include "config.h"
 #include "utils.h"
 
-CRBG leds[NB_PIXELS];
+CRGB leds[NB_PIXELS];
 
 void setup()
 {
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
-  pinMode(PIN_MODE, INPUT_PULLUP);
-  FastLED.addLeds<WS2812, PIN_DATA, GRB>(leds, NB_PIXELS).setCorrection(TypicalLEDStrip);
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
+    pinMode(PIN_MODE, INPUT_PULLUP);
+    FastLED.addLeds<WS2812, PIN_DATA, GRB>(leds, NB_PIXELS).setCorrection(TypicalLEDStrip);
 }
 
 void loop() {
